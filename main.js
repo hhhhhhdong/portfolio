436a6cccd15bc767ba60e912e315d8abae9ba0bc
hhhhhhdong <68246843+hhhhhhdong@users.noreply.github.com>
2020-07-30 13:51:42 +0900

    JS : projects

--- main.js
+++ main.js
@@ -35,6 +35,43 @@ document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHieght;
 });
 
+// arrow up 버튼 스크롤하면 보이게 하기
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHieght) {
+    arrowUp.classList.add("visible");
+  } else {
+    arrowUp.classList.remove("visible");
+  }
+});
+arrowUp.addEventListener("click", () => {
+  console.log("df");
+  scrollIntoView("#home");
+});
+
+// project 필터링
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+workBtnContainer.addEventListener("click", (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+
+  projectContainer.classList.add("anim-out");
+  setTimeout(() => {
+    projects.forEach((project) => {
+      if (filter === "*" || filter === project.dataset.type) {
+        project.classList.remove("invisible");
+      } else {
+        project.classList.add("invisible");
+      }
+    });
+    projectContainer.classList.remove("anim-out");
+  }, 300);
+});
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
